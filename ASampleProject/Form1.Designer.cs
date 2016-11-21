3f62a3f40d472328cf9955ed84a2add2292ed44b
daniel <daniel@here.there>
2016-11-20 22:55:25 -0500

    Renamed listbox Added code that will read the text from the names.txt file on the refresh button click event

--- ASampleProject/Form1.Designer.cs
+++ ASampleProject/Form1.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.refreshButton = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.namesListBox = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // refreshButton
@@ -38,24 +38,24 @@
             this.refreshButton.Name = "refreshButton";
             this.refreshButton.Size = new System.Drawing.Size(95, 29);
             this.refreshButton.TabIndex = 0;
-            this.refreshButton.Text = "Refresh";
+            this.refreshButton.Text = "Refresh List";
             this.refreshButton.UseVisualStyleBackColor = true;
             this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
             // 
-            // listBox1
+            // namesListBox
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 12);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(159, 238);
-            this.listBox1.TabIndex = 1;
+            this.namesListBox.FormattingEnabled = true;
+            this.namesListBox.Location = new System.Drawing.Point(12, 12);
+            this.namesListBox.Name = "namesListBox";
+            this.namesListBox.Size = new System.Drawing.Size(159, 238);
+            this.namesListBox.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.namesListBox);
             this.Controls.Add(this.refreshButton);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -66,7 +66,7 @@
         #endregion
 
         private System.Windows.Forms.Button refreshButton;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox namesListBox;
     }
 }
 
--- ASampleProject/Form1.cs
+++ ASampleProject/Form1.cs
@@ -20,8 +20,32 @@ namespace ASampleProject
 
         private void refreshButton_Click(object sender, EventArgs e)
         {
-
-
+            System.IO.StreamReader file;
+            string line;
+            try
+            {
+                file = new System.IO.StreamReader("names.txt");
+            }
+            catch
+            {
+                MessageBox.Show("Unable to open file: names.txt");
+                return;
+            }
+            int counter = 0;
+            try
+            {
+                while ((line = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                    counter++;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error encountered reading data from file");
+                return;
+            }
+            Console.WriteLine("Read {0} names from the file", counter);
         }
     }
 }
